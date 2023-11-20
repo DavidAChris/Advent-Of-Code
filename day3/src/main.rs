e7a7de2313e32506b107aa25fa833fb1f114c017
DavidAChris <david.christiea@gmail.com>
2023-11-20 14:27:15 -0500

    Day 3 Part 2 Answer

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -8,24 +8,27 @@ fn main() -> Result<(), Box<dyn Error>> {
     let file = OpenOptions::new().read(true).open("input.txt")?;
     let reader = BufReader::new(file);
     let mut total = 0;
+    let mut lines = reader.lines();
 
-    for line in reader.lines() {
-        let line = line?;
-        let len = line.len() / 2;
-        let (first, second) = (&line[..len], &line[len..]);
-        total += priority(first, second);
+    loop {
+        let (first, second, third) = (lines.next(), lines.next(), lines.next());
+        if first.is_none() || second.is_none() || third.is_none() {
+            break;
+        }
+        let (first, second, third) = (first.unwrap()?, second.unwrap()?, third.unwrap()?);
+        total += priority(&first, &second, &third);
     }
 
     println!("Total: {}", total);
     Ok(())
 }
 
-fn priority(first: &str, second: &str) -> i32 {
+fn priority(first: &str, second: &str, third: &str) -> i32 {
     let lower_offset = 96;
     let upper_offset = 38;
     let mut priority = 0;
     for character in first.chars() {
-        if second.contains(character) {
+        if second.contains(character) && third.contains(character) {
             let is_lower = character.is_lowercase();
             if is_lower {
                 priority = character as i32 - lower_offset;
